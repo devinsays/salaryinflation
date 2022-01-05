1e7702c58e9c2595af7535b3ac975a2450984824
Devin Price <devin@wptheming.com>
2022-01-04 20:33:20 -0600

    Remove trailing 0 from percent output.

--- script.js
+++ script.js
@@ -208,7 +208,7 @@ calculateButton.onclick = (e) => {
     // Output values
     resultWage.innerText = result;
     resultAdjustment.innerText = difference;
-    resultPercent.innerText = totalInflation.toFixed(2);
+    resultPercent.innerText = parseFloat( totalInflation.toFixed(2) );
     let output = `A wage of <mark>$${result}</mark> in today's dollars (${mostRecentDataMonth}/${mostRecentDataYear}) is the same as <mark>$${wage}</mark> in ${month}/${year} in terms of purchasing power.`;
     resultText.innerHTML = output;
     resultSection.style.display = 'block';
